7e851ef3d252a712fc72f725f44f90fb29e4fb41
Dave Pagurek <dave@davepagurek.com>
2018-06-14 22:20:06 -0400

    Add random example, multiple seeds

--- src/frontend/client.ts
+++ src/frontend/client.ts
@@ -1,4 +1,6 @@
 import * as calder from 'calder-gl';
+const randomSeed = require('random-seed');
+import { range } from 'lodash';
 
 for (const key in calder) {
     (<any>window)[key] = calder[key];
@@ -19,9 +21,14 @@ if (webglElement === null) {
     throw new Error('Could not find webgl element');
 }
 
+const shuffleBtn = <HTMLButtonElement> document.getElementById('shuffle');
+const form = <HTMLFormElement> document.getElementById('state');
+const seedElements = range(4).map((i) => <HTMLInputElement> document.getElementById(`seed${i}`));
+const focusedElement = <HTMLInputElement> document.getElementById('focused');
+
 const oldLog = console.log;
 console.log = function() {
-    logElement.innerText += Array.prototype.map.call(arguments, (a: any) => `${a}`).join(', ');
+    logElement.innerText += Array.prototype.map.call(arguments, (a: any) => `${a}`).join(', ') + '\n';
     oldLog.apply(console, arguments);
 };
 
@@ -33,7 +40,34 @@ const code = codeElement.innerText;
 
 const setup = new Function('renderer', code);
 
-const renderer = new calder.Renderer(800, 600, 10);
-setup(renderer);
+const oldRandom = Math.random;
+const render = (seed, size, handler) => {
+    const generator = randomSeed.create();
+    generator.seed(seed);
+    Math.random = () => generator.random();
 
-webglElement.appendChild(renderer.stage);
+    const renderer = new calder.Renderer(size, size, 10);
+    setup(renderer);
+
+    webglElement.appendChild(renderer.stage);
+    renderer.stage.addEventListener('click', handler);
+};
+
+const seeds = seedElements.map((e) => parseFloat(e.value));
+const focused = parseInt(focusedElement.value);
+if (focused >= 0 && focused < 4) {
+    render(seeds[focused], 400, () => {
+        focusedElement.value = '-1';
+        form.submit();
+    });
+} else {
+    seeds.forEach((seed, i) => render(seed, 150, () => {
+        focusedElement.value = `${i}`;
+        form.submit();
+    }));
+}
+
+shuffleBtn.addEventListener('click', () => {
+    seedElements.map((e) => e.value = oldRandom() * 10000);
+    form.submit();
+});
